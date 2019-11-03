556d58d84d67fb126fb53a60caa3a80301808f53
Doug Guzman <dgguzman@mail.bradley.edu>
2019-11-03 12:35:22 -0600

    Starts adding the Custom Vision for Azure

---
+++ Assets/Scripts/Vision/CustomVisionAnalyzer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomVisionAnalyzer : MonoBehaviour
+{
+    /// <summary>
+    /// Unique instance of this class
+    /// </summary>
+    public static CustomVisionAnalyzer instance;
+
+    /// <summary>
+    /// The predecetion key for the Azure server
+    /// </summary>
+    private string predictionKey = "3e0f466c3e9342b28a6469f3ed9abdd0";
+
+    /// <summary>
+    /// The URL for the Azure server
+    /// </summary>
+    private string predectionEndpoint = "https://customvisionsuits.cognitiveservices.azure.com/customvision/v3.0/Prediction/9fcd12e5-203d-4f22-b1ac-b203f05cfac8/detect/iterations/ChairTest/url";
+
+    /// <summary>
+    /// Byte array of the image to submit for analysis
+    /// </summary>
+    [HideInInspector] public byte[] imageBytes;
+
+    private void Awake()
+    {
+        // Ses the class up as a singleton
+        instance = this;
+    }
+
+    /// <summary>
+    /// Call the servie to submit the image
+    /// </summary>
+    /// <param name="imagePath">The file path to the image</param>
+    /// <returns></returns>
+    public IEnumerator AnalyzeLasImageCaptured(string imagePath)
+    {
+        WWWForm webForm = new WWWForm();
+
+        using (UnityWebRequest unityWebRequest =
+            UnityWebRequest.Post(predectionEndpoint, webForm))
+        {
+            // Gets a byte array out of the saved image
+            imageBytes = GetImageAsByteArray(imagePath);
+
+            unityWebRequest.SetRequestHeader("Content-Type",
+                "application/json");
+            unityWebRequest.SetRequestHeader("Predection-Key", predictionKey);
+
+            // The upload handler will help uploading the byte array with the request
+            unityWebRequest.uploadHandler = new UploadHandlerRaw(imageBytes);
+            unityWebRequest.uploadHandler.contentType = "application/json";
+
+            // The download handler will help receiving the analysis from Azure
+            unityWebRequest.downloadHandler = new DownloadHandlerBuffer();
+
+            // Send the request
+            yield return unityWebRequest.SendWebRequest();
+            
+            string jsonResponse = unityWebRequest.downloadHandler.text;
+            
+            // Creaete a texture. Texture size does not mattre since LoadImage will replace with the incoming image size
+            //Texture2D tex = new Textture2D(1, 1);
+            //tex.LoadImage(imageBytes);
+            //SceneOrganiser.Istance.quadRenderer.material.SetTexure("_MainTex", tex);
+            
+            // The response will be in JSON forma,therefor it needs to be deserialized
+            //AnalysisRootObject analysisRootObject = new AnalysisRootObject();
+            //analysisRootObject = JsonConvert.DeserializeObject<AnalysisRootObject>(jsonResponse);
+            
+            //Sceneorganizer.Instance.FinaliseLabel(analysisRootObject);
+        }
+    }
+
+    static byte[] GetImageAsByteArray(string imageFilePath)
+    {
+        FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+        
+        BinaryReader binaryReader = new BinaryReader(fileStream);
+
+        return binaryReader.ReadBytes((int) fileStream.Length);
+    }
+}
---
+++ Assets/Scripts/Vision/CustomVisionObject.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// The objects in this script represent the deserialized version of the objects used bby this application
+
+/// <summary>
+/// Web request object for image ddata
+/// </summary>
+class MultipartObject : IMultipartFormSection
+{
+    public string sectionName { get; set;  }
+
+    public byte[] sectionData { get; set;  }
+
+    public string fileName { get; set; }
+
+    public string contentType { get; set;  }
+}
+
+/// <summary>
+/// JSON of all the Tags existing within the project
+/// </summary>
+public class Tags_RootObject
+{
+    public List<TagOfProject> Tags { get; set; }
+    public int TotalTaggedImaged { get; set; }
+    public int TotalUntaggedImaged { get; set; }
+}
+
+public class TagOfProject
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int ImageCout { get; set; }
+}
+
+/// <summary>
+/// JSON of Tag to associate to an image
+/// Contains a list of hosting the tags, since multiple tags can be associated with one image
+/// </summary>
+public class Tag_RootObject
+{
+    
+}
+public class CustomVisionObject : MonoBehaviour
+{
+    
+}
